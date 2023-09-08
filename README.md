6fc3bd9ae1d55fdf28b2c45aff21c18316b7a977
Alpha <alpha@AlphadeMacBook-Pro.local>
2023-09-09 00:08:50 +0800

    deploy version

--- README.md
+++ README.md
@@ -4,7 +4,48 @@ Just for fun......
 
 # How to use
 
-```sh
+## sh
+
+````sh
 npm install
 npm start
-```
+````
+
+## pm2
+
+```` pm2
+npm install pm2@latest -g
+pm2 start --name dayu-app 'npm start'
+pm2 list
+````
+
+## nginx
+
+### path
+
+````
+cd /etc/nginx/conf.d
+````
+
+### config
+
+````
+server {
+        listen       80;
+        server_name  localhost;
+
+        location / {
+            proxy_pass http://localhost:3000;
+        }
+	
+	location ^~ /video/ {
+            alias /var/videos/;  # 音视频文件目录
+    	}
+}
+````
+
+### restart
+
+````
+systemctl restart nginx
+````
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,41 @@
 import React, {useEffect, useState} from "react";
-import {Container, Nav, Navbar, ToggleButton} from "react-bootstrap";
+import {Nav, Navbar, ToggleButton} from "react-bootstrap";
 import Image from "next/image";
 import Logo from "../static/logo.png";
-import i18next from "i18next";
 import {useTranslation} from "next-i18next";
-import Link from "next/link";
 import {useRouter} from "next/router";
 
 const Header: React.FC = () => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const router = useRouter();
 
     const [radioValue, setRadioValue] = useState('1');
+    const [currentNav, setCurrentNav] = useState('');
 
     useEffect(() => {
-        console.log(router.asPath)
-        if(router.asPath && router.asPath.startsWith("/#")){
-            router.replace("/")
+        if (router.asPath && router.asPath.startsWith("/#")) {
+            console.log("/#")
+            router.push("/", "/", {locale: 'zh'})
         }
-        if(radioValue === '1' && router.locale !== 'zh'){
-            router.replace(router.asPath, router.asPath, { locale: 'zh'})
+        if (radioValue === '1' && router.locale !== 'zh') {
+            console.log("route to zh")
+            router.push("/" + currentNav, "/" + currentNav, {locale: 'zh'})
             router.locale = 'zh'
-        }else if(radioValue === '2' && router.locale !== 'en'){
-            router.replace(router.asPath, router.asPath, { locale: 'en'})
+        } else if (radioValue === '2' && router.locale !== 'en') {
+            console.log("route to en")
+            router.push("/" + currentNav, "/" + currentNav, {locale: 'en'})
             router.locale = 'en'
         }
     }, [radioValue]);
 
-
     const locales = [
-        { name: '中', value: '1' },
-        { name: 'En', value: '2' },
+        {name: '中', value: '1'},
+        {name: 'En', value: '2'},
     ];
+
+    const clickNavItem = (e) => {
+        setCurrentNav(e.currentTarget.getAttribute("href"))
+    }
     return (
         <Navbar expand="lg" fixed={"top"} bg="dark" data-bs-theme="dark">
             <Navbar className="">
@@ -42,39 +46,43 @@ const Header: React.FC = () => {
                     }}/>
                 </Navbar.Brand>
             </Navbar>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end bg-dark position-relative">
                 <Nav variant="underline" defaultActiveKey="#companyProfile">
                     <Nav.Item>
-                        <Nav.Link href="#companyProfile">{t("header:companyProfile")}</Nav.Link>
+                        <Nav.Link href="#companyProfile"
+                                  onClick={(e) => clickNavItem(e)}>{t("header:companyProfile")}</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="#ability">{t("header:productService")}</Nav.Link>
+                        <Nav.Link href="#ability"
+                                  onClick={(e) => clickNavItem(e)}>{t("header:productService")}</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="#researchHistory">{t("header:techCapability")}</Nav.Link>
+                        <Nav.Link href="#researchHistory"
+                                  onClick={(e) => clickNavItem(e)}>{t("header:techCapability")}</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="#achievement">{t("header:achievement")}</Nav.Link>
+                        <Nav.Link href="#achievement"
+                                  onClick={(e) => clickNavItem(e)}>{t("header:achievement")}</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="#aboutUs">{t("header:contactUs")}</Nav.Link>
+                        <Nav.Link href="#aboutUs" onClick={(e) => clickNavItem(e)}>{t("header:contactUs")}</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                    {locales.map((radio, idx) => (
-                        <ToggleButton
-                            key={idx}
-                            id={`radio-${idx}`}
-                            type="radio"
-                            variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-                            name="radio"
-                            value={radio.value}
-                            checked={radioValue === radio.value}
-                            onChange={(e) => setRadioValue(e.currentTarget.value)}
-                        >
-                            {radio.name}
-                        </ToggleButton>
-                    ))}
+                        {locales.map((radio, idx) => (
+                            <ToggleButton
+                                key={idx}
+                                id={`radio-${idx}`}
+                                type="radio"
+                                variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                                name="radio"
+                                value={radio.value}
+                                checked={radioValue === radio.value}
+                                onChange={(e) => setRadioValue(e.currentTarget.value)}
+                            >
+                                {radio.name}
+                            </ToggleButton>
+                        ))}
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
